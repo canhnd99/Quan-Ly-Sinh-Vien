df051709fe4fda0e7aa50b98fd18d75eacd673f3
DucCanh <duccanh24599@gmail.com>
2019-11-10 21:41:35 +0700

    tao lop ConnectToDatabase de lien ket voi csdl

--- src/connections/ConnectToDatabase.java
+++ src/connections/ConnectToDatabase.java
@@ -1,5 +1,21 @@
 package connections;
 
-public class ConnectToDatabase {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
+public class ConnectToDatabase {
+    public Connection getConnection(){
+        String url = "jdbc:mysql://localhost/sms";
+        String user = "root";
+        String pass = "1234";
+        try {
+            Connection cnt = DriverManager.getConnection(url, user, pass);
+            return cnt;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
 }
---
+++ src/connections/Test.java
@@ -0,0 +1,9 @@
+package connections;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ConnectToDatabase db = new ConnectToDatabase();
+        db.getConnection();
+    }
+}
