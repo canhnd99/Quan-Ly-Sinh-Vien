cddb1cedbe5597f33f3507fd9600cb63ad4dfba0
DucCanh <duccanh24599@gmail.com>
2019-11-15 21:16:30 +0700

    tạo đối tượng Student

--- src/models/Student.java
+++ src/models/Student.java
@@ -1,15 +1,74 @@
 package models;
 
+import java.sql.Date;
+
 public class Student {
+
+    private String id;
     private String name;
-    private int age;
+    private String gender;
+    private Date birthDate;
+    private String phoneNum;
     private String address;
-    
-    public Student(){}
-    public Student(String name, int age, String address){
+
+    public Student() {
+    }
+
+    public Student(String id, String name, String gender, Date birthDate, String phoneNum, String address) {
+        this.id = id;
         this.name = name;
-        this.age = age;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.phoneNum = phoneNum;
         this.address = address;
-    } 
-    
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
 }
