470d82ee938615161ffc3a628b7f1ee8d7098e37
DucCanh <duccanh24599@gmail.com>
2019-11-15 07:40:51 +0700

    huy commit

--- src/models/Student.java
+++ src/models/Student.java
@@ -1,5 +1,15 @@
 package models;
 
 public class Student {
-
+    private String name;
+    private int age;
+    private String address;
+    
+    public Student(){}
+    public Student(String name, int age, String address){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    } 
+    
 }
